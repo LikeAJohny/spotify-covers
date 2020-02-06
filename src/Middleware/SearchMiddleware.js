fa19157b047731b04ba3d2cbf0fa90722a92d1b0
Jonas <jo@v2a.net>
2020-02-06 14:22:49 +0100

    SearchMiddleware acting async to await the search result

--- src/Middleware/SearchMiddleware.js
+++ src/Middleware/SearchMiddleware.js
@@ -1,20 +1,8 @@
 const SearchSpotify = require('../Service/SearchSpotify.js');
 
-const SearchMiddleware = (req, res, next) => 
+const SearchMiddleware = async (req, res, next) => 
 {
-    const searchTerm = req.body.searchTerm;
-    let result;
-
-    SearchSpotify.search(searchTerm)
-        .then((data) => {
-            result = data;
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
-    console.log(result);
-    req.searchResult = result;
+    req.searchResult = await SearchSpotify.search(req.body.searchTerm);
 
     next();
 };
