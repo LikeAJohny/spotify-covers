b959e7d5ad64930fd10da1c4642194bed472642a
Jonas <jo@v2a.net>
2020-02-06 14:22:28 +0100

    rewrite SearchSpotify

--- src/Service/SearchSpotify.js
+++ src/Service/SearchSpotify.js
@@ -2,23 +2,21 @@ const axios = require('axios');
 const token = require('../Storage/TokenStorage.js').read();
 
 const search = async (searchTerm) => {
-    return axios(
-        {
-            method: 'GET',
-            url: `https://api.spotify.com/v1/search?q=${searchTerm}&type=artist,album,track&market=DE`,
-            headers: {
-                'Authorization': 'Bearer ' + token.accessToken,
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },        
-        }
-    )
-    .then((response) => {
-        return response.data;
-    })
-    .catch((err) => {
-        console.log(err.response);    
-    });
+    try {
+        return await axios(
+            {
+                method: 'GET',
+                url: `https://api.spotify.com/v1/search?q=${searchTerm}&type=artist,album,track&market=DE`,
+                headers: {
+                    'Authorization': 'Bearer ' + token.accessToken,
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                },        
+            }
+        ).then(result => result.data);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 module.exports = { search };
