336445ec18a50c2558f21a5e107b70571d9e591b
Jonas <jo@v2a.net>
2020-02-07 15:38:54 +0100

    reorganize routes; everything's going through / now

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const BodyParserMiddleware = require('body-parser');
 const NotFoundHandler = require('../src/Handler/NotFoundHandler.js');
-const HomePageHandler = require('../src/Handler/HomePageHandler.js');
 const AuthMiddleware = require('../src/Middleware/AuthMiddleware.js');
 const SearchResultHandler = require('../src/Handler/SearchResultHandler.js');
 const SearchMiddleware = require('../src/Middleware/SearchMiddleware.js');
@@ -14,15 +13,9 @@ router.use(BodyParserMiddleware.urlencoded({ extended: false }));
 router.use(BodyParserMiddleware.json());
 
 // HomePage
-/*
- * Hat keine Auswirkungen, da index.html wohl Standard mäßig gerendert wird?!
- * ToDo: Checken wie das läuft.
- */
-router.get('/', HomePageHandler);
-
-// General Spotify Search
-router.get('/search', SearchResultHandler);
-router.post('/search', [SearchMiddleware, SearchResultHandler]);
+router.get('/', SearchResultHandler);
+// Search Request
+router.post('/', [SearchMiddleware, SearchResultHandler]);
 
 router.use(NotFoundHandler);
 
